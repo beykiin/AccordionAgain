0f1b86aa11288be35098d236e6df5dcb73a080e9
Yasin Beken <yasinbeken16@gmail.com>
2024-03-01 17:19:42 +0300

    First Commit

---
+++ accordionAgain.js
@@ -0,0 +1,22 @@
+let accordions=document.getElementsByClassName("accordion")
+
+for(let i=0;i<accordions.length;i++){
+
+    accordions[i].addEventListener("click",function(){
+        this.classList.toggle("active")
+        let panel=this.nextElementSibling;
+        // if(panel.style.display=="block"){
+        //     panel.style.display="none"
+        // }
+        // else{
+        //     panel.style.display="block"
+        // }
+        if(panel.style.maxHeight){
+            panel.style.maxHeight=null
+        }
+        else{
+            panel.style.maxHeight=panel.scrollHeight+"px"
+        }
+    })
+
+}
